5541831c8f8a5197dc4812f3217c0b576c278683
tm1990 <haavardpc@hotmail.com>
2014-06-12 23:13:24 +0200

    Fixes for the build script

--- src/main/java/com/miscitems/MiscItemsAndBlocks/Utils/Refrence.java
+++ src/main/java/com/miscitems/MiscItemsAndBlocks/Utils/Refrence.java
@@ -7,7 +7,7 @@ public class Refrence {
 	
 	public static final String Mod_Name = "MiscItemsAndBlocks";
 	public static final String Mod_Id = "MiscItems";
-	public static final String Version = "${version}";
+	public static final String Version = "@VERSION@";
 
     
     public static final String Channel = "MiscItems";
